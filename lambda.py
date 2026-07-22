981be3aba3375056ff45a690182ec66857070a85
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-22 07:27:38 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,8 @@
+
+numbers = [4, 9, 15, 22, 30, 7, 18]
+
+filter_number = list(filter(lambda x: x > 10, numbers))
+map_number = list(map(lambda x: x * 2, numbers))
+
+print(filter_number)
+print(map_number)
