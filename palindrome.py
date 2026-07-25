8a10ffeb2906b38dcb9a62411b9761056351b4d7
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-25 19:13:32 +0530

    Add palindrome checking functionality
    Implement palindrome check by reversing a string.

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+
+#palindrome
+user = "madam"
+reveresed = ""
+
+for i in user : 
+    
+    reveresed = i +  reveresed
+print(reveresed)
+
+
+if reveresed == user:
+    print ("done")
+else:
+    print ("no ")
